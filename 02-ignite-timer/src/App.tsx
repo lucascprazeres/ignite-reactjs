ec09db823aec2949fb067a32b5a41c5eaa76f73b
Lucas Prazeres <lucascprazeres@outlook.com>
2024-01-05 18:18:04 -0300

    chore(modulo-02): setup do projeto

---
+++ 02-ignite-timer/src/App.tsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button color="primary" />
+      <Button color="secondary" />
+      <Button color="danger" />
+      <Button color="success" />
+      <Button />
+    </ThemeProvider>
+  );
+}
